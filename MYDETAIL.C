4469bd606b7e29491a6a00fab86291114bfc34b2
rahulbaghel1707 <rahulbaghel7146@gmail.com>
2025-01-20 09:51:00 +0530

    20-01-2025

---
+++ MYDETAIL.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int age,dob;
+	char *name,*add;
+	clrscr();
+	printf("\nmy details :\n");
+	printf("\nName : rahul baghel");
+	printf("\ndob  : 17/07/2004 ");
+	printf("\nage  :20");
+	printf("\nadd :naroda ahmedabad");
+	printf("\nthanks you......");
+	getch();
+}
